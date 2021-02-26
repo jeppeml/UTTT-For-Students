106eaaa7ab578b2b5cde2188e1be11a0fda7f40b
jeppeml <jle@easv.dk>
2021-02-26 09:47:15 +0100

    Removed NetBeans config
    Updated everything to work with Java 15 and IntelliJ

--- src/dk/easv/gui/util/FontAwesomeHelper.java
+++ src/dk/easv/gui/util/FontAwesomeHelper.java
@@ -7,6 +7,7 @@ package dk.easv.gui.util;
 // https://bintray.com/jerady/maven/FontAwesomeFX/8.15#files/de%2Fjensd%2Ffontawesomefx-commons%2F8.15
 // https://bintray.com/jerady/maven/FontAwesomeFX/8.15#files/de%2Fjensd%2Ffontawesomefx-fontawesome%2F4.7.0-5
 // https://bintray.com/jerady/maven/FontAwesomeFX/8.15#files/de%2Fjensd%2Ffontawesomefx-materialdesignfont%2F1.7.22-4
+import de.jensd.fx.glyphs.GlyphsBuilder;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
 import de.jensd.fx.glyphs.fontawesome.utils.FontAwesomeIconFactory;
 import javafx.scene.text.Text;
@@ -19,6 +20,7 @@ public class FontAwesomeHelper {
     public static Text getFontAwesomeIconFromPlayerId(String playerId) throws RuntimeException {
         switch (playerId) {
             case "0":
+
                 return FontAwesomeIconFactory.get().createIcon(FontAwesomeIcon.DIAMOND);
             case "1":
                 return FontAwesomeIconFactory.get().createIcon(FontAwesomeIcon.TRASH);
